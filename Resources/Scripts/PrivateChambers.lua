3bdcff6ca5c14c6a0a17857f97a79d10550c739e
Use After Free <use.after.free.177@gmail.com>
2020-10-23 02:11:49 +0200

    Done Lua TODO items.

--- Resources/Scripts/PrivateChambers.lua
+++ Resources/Scripts/PrivateChambers.lua
@@ -12,9 +12,9 @@ function Drinks(girl, dinner_already)
         if choice == 0 then
             Dialog("You sense an opportunity in her inebriated state.  You start rubbing her shoulders and back.  Soon your hands are roaming towards her breasts...")
             if wm.Percent(60) then
+                PlayerfucksGirl(girl)
                 Dialog("She lets out a soft moan as your fingers caress her nipples thru the fabric of her top. Your other hand sneaks along her inner thigh;  meeting no resistance your fingers slide easily under her panties and your stimulate her clitoris.")
                 Dialog("Before long you are both kissing and probing each other...having lost all sense of time you come to your senses panting and gasping on the floor by your couch.  You look down to see that she her breathing heavily and deeply in an exhausted sleep.")
-                -- TODO normal sex
             else
                 Dialog("A sharp smack on your hand and a harsh look in her eyes, tells you that your gamble has failed.  She regains a little sobriety and demands you take her back to her room.")
                 Dialog("You escort her back to her room and you both pause just outside her door...")
@@ -59,9 +59,9 @@ function HerRoom(girl)
             Dialog("As your lips come together you slide your tongue into her mouth...")
             if girl:libido() >= 45 then
                 Dialog("Her tongue meets yours and they begin a swirling dance back and forth. After several seconds the kiss ends with her gently biting your bottom lip as you separate.")
+                PlayerFucksGirl(girl)
                 Dialog("She takes your hands and leads you silently inside her room and toward her bed.")
                 Dialog("The sounds of your lovemaking can be heard throughout the neighborhood.  A perfect ending to a perfect evening.")
-                -- TODO normal sex
             else
                 Dialog("She kisses you back passively.  After a few moments you separate and say your final good nights.")
             end
@@ -69,10 +69,11 @@ function HerRoom(girl)
             Dialog("You decide to not try using your tongue and your lips come together in a kiss...")
             if girl:libido() >= 65 then
                 Dialog("A moment later you find her tongue sliding past your lips and you reciprocate passionately...")
+                wm.UpdateImage(wm.IMG.ORAL)
                 Dialog("A trail of saliva hangs between you lips as you separate.  She kisses your neck unbuttoning your shirt; She kisses your chest and continues moving downward.  \"I'm still a little hungry.\" She whispers.  you revel in the feeling as she kisses the head of your dick and begins to lick and suck you hungrily.  You finish quickly under her onslaught and she gulps down every last drop.")
                 Dialog("You stand reeling from the pleasure,  She lays back on the bed removing her lingerie and spreading her legs wide.  \"Are you still hungry?\" She asks spreading her pussy lips wide with her fingers.")
                 Dialog("You smile and dive into the task before you.  You awake the next morning holding her tightly in her bed.")
-                -- TODO oral sex
+                girl:oral(2)
             else
                 Dialog("You kiss her lips and and say good night.")
             end
@@ -92,8 +93,9 @@ function SheIsAsleep(girl)
         Dialog("You pulse quickens as her panties slowly slide down.  Halfway down her ass....her upper thighs...her knees...calves...you stop and leave them at her ankles and begin to slide your fingers into her vagina as you stroke your cock.")
         if wm.Percent(40) then
             Dialog("She moans sleepily as you rub the head of your penis against her juicy labia,  Slowly you begin to push inside her.  You move slowly and deliberately enjoying the feeling.  She gasps repeatedly and her mouth hangs open slightly, giving you an idea.")
+            wm.UpdateImage(wm.IMG.ORAL)
             Dialog("You pull out of her pussy and bring your dick to her mouth.  gently you slide it past her lips.  The sensation and tension of the situation feel amazing and you release a large amount of cum down her throat.  Still sleeping she swallows it down. After a few moments you carefully put her closes back on and leave her to sleep.")
-            -- TODO oral sex
+            girl:oral(2)
         else
             Dialog("She awakes with a sudden start and stares wide eyed at your fingers inside her and your erection moving closer.  \"What the fuck!\" She cries \"Get off of me!\"")
             local action = ChoiceBox("", "Hold her down and Fuck her anyway.", "Apologize and Leave")
@@ -130,10 +132,11 @@ function Dinner(girl)
         Dialog("You call for the chef to bring out the dessert course.")
         if girl:libido() > 80 then
             Dialog("The Creme Brulee this evening is exquisite. You become lost in each others eyes,  in fact, you become so distracted that you accidentally drop a spoonful of the sweet substance in your lap.  You apologize and move to clean it up, but she stops you saying \"Let me get that for you.\"")
+            wm.UpdateImage(wm.IMG.ORAL)
             Dialog("She kneels beside you and moves her face very close to your lap.  She begins to lick the creme from your crotch hungrily.  After a few moments your pants are clean, but she looks up at you poutingly \"Is that all there was?  I want more cream.\"")
             Dialog("A devilish grin graces her face and she begins to remove your already rock hard erection from your trousers.  She licks and sucks greedily and expertly.  You feel the wave of cream swell within you and you release a massive explosion in her mouth..")
             Dialog("She swallows it all down in one gulp and licks her lips.  \"Now that was really satisfying.\"  she says with that devilish grin.  She thanks you for the wonderful evening and returns to her room.")
-            -- TODO oral sex
+            girl:oral(2)
             girl:happiness(3)
             girl:pclove(2)
             girl:pchate(-2)
@@ -141,7 +144,7 @@ function Dinner(girl)
             Dialog("\"Wait\" She says \"I want to make you a dessert myself.\"  She excuses herself to the kitchen for a few moments.")
             Dialog("You nearly fall out of your chair when she returns wearing nothing but whipped cream lingerie with cherries over her nipples. \"I wanted to make you a banana split, but I couldn't find a good banana in the kitchen.\" she smiles coyly \"Do you know where I can find a banana?\"")
             Dialog("\"I think I can help you out with that\" you reply as you approach her.  You spend the rest of the evening applying and removing whipped cream from each others bodies.")
-            -- TODO normal sex
+            PlayerFucksGirl(girl)
             girl:happiness(3)
             girl:pclove(2)
             girl:pchate(-2)
@@ -152,7 +155,7 @@ function Dinner(girl)
             Dialog("You both both sit back heavily as you finish the rather large portions of cake.  She thanks you for the food and begins to leave.  You contemplate asking her to stay, but realize that any activity would likely cause both of you to explode, and not in a good way.")
         end
     elseif choice == 1 then
-        Dialog("You gently take her by the hand and walk back to her room ,arm in arm.")
+        Dialog("You gently take her by the hand and walk back to her room, arm in arm.")
         return HerRoom(girl)
     else
         Dialog("You invite her to join you on the couch for some after dinner drinks.")
@@ -175,14 +178,15 @@ function AfterDinner(girl)
             -- GOTO 555 // 8
         elseif girl:pclove() > 50 then
             Dialog("She looks deep into your eyes. \"Of course, my love, I have to properly thank you for this lovely evening...\"")
-            -- TODO normal sex
+            PlayerFucksGirl(girl)
         elseif girl:libido() > 60 then
             Dialog("She giggles a bit as she looks up at you.  \"I've always wondered what the Master's bedroom looks like.\"")
             Dialog("She adds with a wink \"Perhaps we could both enjoy some tossed salad on your bed.\"")
-            -- TODO anal sex
+            wm.UpdateImage(wm.IMG.ANAL)
+            girl:anal(2)
         elseif wm.Percent(50) then
             Dialog("\"How can I refuse the offer of an even more delicious dessert?\" She replies in a sultry voice.")
-            -- TODO normal sex
+            PlayerFucksGirl(girl)
         else
             Dialog("\"That is a lovely offer,\" She replies \"but I think it's best if I return to my room.\"")
             return HerRoom(girl)
